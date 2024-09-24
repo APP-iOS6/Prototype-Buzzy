01f43d38a136cc1c151847eab556682e31cd357f
Min <jmsj0308@naver.com>
2024-09-24 16:48:38 +0900

    add: SettingsView 파일 추가

---
+++ Buzzy/Buzzy/Presentation/MyPage/SettingsView.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsView.swift
+//  Buzzy
+//
+//  Created by Min on 9/24/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("SettingsView")
+    }
+}
+#Preview {
+    SettingsView()
+}
