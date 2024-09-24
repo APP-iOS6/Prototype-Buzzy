4ff3b655d965b7bd590c281df96b7d27dc83bac0
Wonho Kim <dnjsgh4829@hs.ac.kr>
2024-09-24 22:21:59 +0900

    [style]: 뷰에 따라서 리스트 스타일 변경

--- Buzzy/Buzzy/Presentation/Home/HomeRankingView.swift
+++ Buzzy/Buzzy/Presentation/Home/HomeRankingView.swift
@@ -14,10 +14,12 @@ struct HomeRankingView: View {
     //임시색상
     let customGray = Color(red: 68 / 255, green: 68 / 255, blue: 68 / 255)
     
+    
     var body: some View {
         NavigationView {
             VStack {
                 HStack {
+
                     Text("알바 랭킹")
                         .font(.bold24)
                     
@@ -40,7 +42,7 @@ struct HomeRankingView: View {
                     .padding(5)
                     .background(selectedButton == 0 ? customGray : Color.clear) // 선택된 경우 검정색 배경
                     .foregroundColor(selectedButton == 0 ? .white : .black) // 선택된 경우 흰색 글자
-                    .cornerRadius(10) // 모서리 반경 설정
+                    .cornerRadius(10)
                     .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 1))
                     
                     Button("평점순") {
@@ -54,8 +56,8 @@ struct HomeRankingView: View {
                     .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 1))
                     
                     Button("리뷰순") {
-                        selectedButton = 2 // 세 번째 버튼 선택
-                        viewModel.sortRanks(by: .default) // 기본 정렬 (원래 순서)
+                        selectedButton = 2
+                        viewModel.sortRanks(by: .byViews) // 조회수랑 같게 함
                     }
                     .padding(5)
                     .background(selectedButton == 2 ? customGray : Color.clear)
@@ -68,7 +70,8 @@ struct HomeRankingView: View {
                 .padding(.horizontal)
                 
                 // 랭킹 리스트 뷰
-                HomeRankingListView(viewModel: viewModel)
+                HomeRankingListView(viewModel: viewModel, showShadowAndSeparator: false)
+                
                 
                 NavigationLink(destination: HomeRankingDetailView()) {
                     Text("랭킹 더 보기")
