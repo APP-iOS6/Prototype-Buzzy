3733229c9eaa4939e5f1bb2813e3484b8616fd32
Min <jmsj0308@naver.com>
2024-09-24 11:17:15 +0900

    feat: 마이페이지 버튼들 추가

--- Buzzy/Buzzy/Presentation/MyPage/MyPageView.swift
+++ Buzzy/Buzzy/Presentation/MyPage/MyPageView.swift
@@ -11,46 +11,100 @@ struct MyPageView: View {
     var body: some View {
         
         NavigationStack {
-            VStack(alignment: .leading, spacing: 16) {
-                
-                HStack(spacing: 16) {
-                    Image("1")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 80, height: 80)
-                        .clipShape(Circle())
-                        .overlay(
-                            Circle().stroke(Color.black, lineWidth: 1))
+            ScrollView {
+                VStack(alignment: .leading, spacing: 16) {
                     
-                    Text("열정알바")
-                        .font(.title)
-                        .fontWeight(.bold)
-                }
-                
-                
-                .padding(.bottom, 20)
-                
-                HStack {
-                    Spacer()
-                    
-                    Button {
+                    HStack(spacing: 16) {
+                        Image("1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                            .overlay(
+                                Circle().stroke(Color.black, lineWidth: 1))
                         
-                    } label: {
-                        Text("지원서 관리")
-                            .font(.headline)
+                        Text("열정알바")
+                            .font(.title)
+                            .fontWeight(.bold)
                     }
-                    .frame(width: 300, height: 40)
-                    .background(.yellow)
-                    .cornerRadius(10)
-
-                    Spacer()
+                    .padding(.top, 32)
+                    
+                    
+                    .padding(.bottom, 20)
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button {
+                            
+                        } label: {
+                            Text("지원서 관리")
+                                .font(.headline)
+                        }
+                        .frame(width: 300, height: 40)
+                        .background(.yellow)
+                        .cornerRadius(10)
+                        
+                        Spacer()
+                    }
+                    .padding(.bottom, 25)
+                    
+                    VStack(alignment: .leading, spacing: 20) {
+                        
+                        NavigationLink {
+                            
+                        } label: {
+                            Text("내가 작성한 글")
+                                .font(.system(size: 20))
+                        }
+                        Divider()
+                        
+                        NavigationLink {
+                            
+                        } label: {
+                            Text("내가 작성한 댓글")
+                                .font(.system(size: 20))
+                        }
+                        Divider()
+                        
+                        NavigationLink {
+                            
+                        } label: {
+                            Text("지역 설정")
+                                .font(.system(size: 20))
+                        }
+                        Divider()
+                        
+                        NavigationLink {
+                            
+                        } label: {
+                            Text("알림설정")
+                                .font(.system(size: 20))
+                        }
+                        Divider()
+                        
+                        
+                        NavigationLink {
+                            
+                        } label: {
+                            Text("고객센터")
+                                .font(.system(size: 20))
+                        }
+                        Divider()
+                        
+                        NavigationLink {
+                            
+                        } label: {
+                            Text("환경설정")
+                                .font(.system(size: 20))
+                        }
+                        Divider()
+                    }
                 }
-                
-                
-                
-                
             }
+            Spacer()
         }
+        .padding(.horizontal, 24)
     }
 }
 
