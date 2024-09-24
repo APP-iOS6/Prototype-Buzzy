01f5508188bcf6dc2890398bf2f4d85476863f48
Juno Lee <juno7746@naver.com>
2024-09-24 09:38:28 +0900

    Add: RecruitView

---
+++ Buzzy/Buzzy/Presentation/Recruit/RecruitView.swift
@@ -0,0 +1,18 @@
+//
+//  RecruitView.swift
+//  Buzzy
+//
+//  Created by Juno Lee on 9/24/24.
+//
+
+import SwiftUI
+
+struct RecruitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecruitView()
+}
