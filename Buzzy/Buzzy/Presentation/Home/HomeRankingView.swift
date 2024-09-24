b6e6dbfc708f480b161186408461b3d451099687
Wonho Kim <dnjsgh4829@hs.ac.kr>
2024-09-24 10:35:34 +0900

    feat: 홈랭킹뷰 만드는중..

--- Buzzy/Buzzy/Presentation/Home/HomeRankingView.swift
+++ Buzzy/Buzzy/Presentation/Home/HomeRankingView.swift
@@ -5,3 +5,29 @@
 //  Created by wonhoKim on 9/24/24.
 //
 
+import SwiftUI
+
+struct HomeRankingView: View {
+    var body: some View {
+        //홈화면에 들어갈 랭킹페이지 및 상세랭킹페이지 만들기
+        VStack {
+            HStack{
+                //padding()
+                Text("알바 랭킹").font(.subheadline)
+                    
+                Spacer()
+                
+                Button {
+                    //랭킹 상세 페이지 뷰 와야함
+                } label: {
+                    Image(systemName: "greaterthan").foregroundStyle(Color.black)
+                }
+
+            }.padding()
+        }
+    }
+}
+
+#Preview {
+    HomeRankingView()
+}
---
+++ Buzzy/Buzzy/Presentation/Home/HomeRankingViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  HomeRankingViewModel.swift
+//  Buzzy
+//
+//  Created by wonhoKim on 9/24/24.
+//
+
