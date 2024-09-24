e7608bdb3b3dd4546b2b21a4b794f78bbba87460
Min <jmsj0308@naver.com>
2024-09-24 10:34:47 +0900

    feat 프로필 이미지 기능 추가 중

--- Buzzy/Buzzy/Presentation/MyPage/MyPageView.swift
+++ Buzzy/Buzzy/Presentation/MyPage/MyPageView.swift
@@ -5,3 +5,27 @@
 //  Created by Min on 9/24/24.
 //
 
+import SwiftUI
+
+struct MyPageView: View {
+    var body: some View {
+        
+        NavigationStack {
+            VStack(alignment: .leading, spacing: 16) {
+                
+                HStack(spacing: 16) {
+                    
+                    Circle()
+                    
+                }
+                
+                
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    MyPageView()
+}
