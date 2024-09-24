458999b0d4b857793195a7bf091fde89af6d48d7
Min <jmsj0308@naver.com>
2024-09-24 11:36:34 +0900

    add: 프로파일 디테일 뷰 추가

---
+++ Buzzy/Buzzy/Presentation/MyPage/ProfileDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  Untitled.swift
+//  Buzzy
+//
+//  Created by Min on 9/24/24.
+//
+
+import SwiftUI
+
+struct ProfileDetailsView: View {
+    var body: some View {
+        Text("123")
+    }
+}
+
+#Preview {
+    ProfileDetailsView()
+}
