2905b0dee41c8ddcf4b5fd0e3e5e5c94dd9c079e
Min <jmsj0308@naver.com>
2024-09-24 12:28:16 +0900

    add: MyPostsView 파일 추가

---
+++ Buzzy/Buzzy/Presentation/MyPage/MyPostsView.swift
@@ -0,0 +1,18 @@
+//
+//  MyPostsView.swift
+//  Buzzy
+//
+//  Created by Min on 9/24/24.
+//
+
+import SwiftUI
+
+struct MyPostsView: View {
+    var body: some View {
+        Text("123")
+    }
+}
+
+#Preview {
+    MyPostsView()
+}
