3cd1e9dfe3df64d436134ed515cfb8144ee81304
Min <jmsj0308@naver.com>
2024-09-24 14:21:17 +0900

    add: MyWalletPoingView 파일 추가

---
+++ Buzzy/Buzzy/Presentation/MyPage/MyWalletPointView.swift
@@ -0,0 +1,18 @@
+//
+//  MyWalletPointView.swift
+//  Buzzy
+//
+//  Created by Min on 9/24/24.
+//
+
+import SwiftUI
+
+struct MyWalletPointView: View {
+    var body: some View {
+        Text("MyWalletPointView")
+    }
+}
+
+#Preview {
+    MyWalletPointView()
+}
