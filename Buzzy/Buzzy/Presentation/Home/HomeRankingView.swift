4945fabc3a1dee83b8ccbbe5e403044f2b009da8
Wonho Kim <dnjsgh4829@hs.ac.kr>
2024-09-25 15:54:55 +0900

    feat: 필터링 및 지역 설정 시트 추가

--- Buzzy/Buzzy/Presentation/Home/HomeRankingView.swift
+++ Buzzy/Buzzy/Presentation/Home/HomeRankingView.swift
@@ -7,10 +7,12 @@
 
 import SwiftUI
 
+//현재 홈 화면쪽에 지역설정 시트가 있는데 더보기 쪽으로 옮길 예정인데, 시트를 사용할지 안할지 의견부탁드립니다
 struct HomeRankingView: View {
     
     @StateObject private var viewModel = HomeRankingViewModel()
     @State private var selectedButton: Int = 0
+    @State private var isSheetPresented = false
     //임시색상
     let customGray = Color(red: 68 / 255, green: 68 / 255, blue: 68 / 255)
     
@@ -36,65 +38,77 @@ struct HomeRankingView: View {
                     }
                 }
                 .padding(.horizontal)
+                
                 //코드 줄이고 싶다
-                ScrollView(.horizontal){
+               
                     HStack {
-                        
-                        Button("조회수 급상승") {
-                            selectedButton = 0 // 첫 번째 버튼 선택
-                            viewModel.sortRanks(by: .byViews) // 조회수로 정렬
+                        Button {
+                            isSheetPresented.toggle()
+                        } label: {
+                            Image(systemName: "gearshape")
                         }
-                        .padding(5)
-                        .background(selectedButton == 0 ? customGray : Color.clear) // 선택된 경우 검정색 배경
-                        .foregroundColor(selectedButton == 0 ? .white : .black) // 선택된 경우 흰색 글자
-                        .cornerRadius(10)
-                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 1))
-                        
-                        Button("평점순") {
-                            selectedButton = 1 // 두 번째 버튼 선택
-                            viewModel.sortRanks(by: .byRating) // 평점순으로 정렬
+                        .sheet(isPresented: $isSheetPresented) {
+                            ResgionSelectSheetView().presentationDetents([.fraction(2/5)])
                         }
-                        .padding(5)
-                        .background(selectedButton == 1 ? customGray : Color.clear)
-                        .foregroundColor(selectedButton == 1 ? .white : .black)
-                        .cornerRadius(10)
-                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 1))
-                        
-                        Button("리뷰순") {
-                            selectedButton = 2
-                            viewModel.sortRanks(by: .byViews) // 조회수랑 같게 함
+
+                        ScrollView(.horizontal, showsIndicators: false){
+                            HStack{
+                                Button("조회수 급상승") {
+                                    selectedButton = 0
+                                    viewModel.sortRanks(by: .byViews) // 조회수로 정렬
+                                }
+                                .padding(5)
+                                .background(selectedButton == 0 ? Color.accentColor : Color.clear)
+                                .foregroundColor(selectedButton == 0 ? .white : .black)
+                                .cornerRadius(10)
+                                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.accentColor, lineWidth: 1))
+                                
+                                Button("평점순") {
+                                    selectedButton = 1
+                                    viewModel.sortRanks(by: .byRating)
+                                }
+                                .padding(5)
+                                .background(selectedButton == 1 ? Color.accentColor : Color.clear)
+                                .foregroundColor(selectedButton == 1 ? .white : .black)
+                                .cornerRadius(10)
+                                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.accentColor, lineWidth: 1))
+                                
+                                Button("리뷰순") {
+                                    selectedButton = 2
+                                    viewModel.sortRanks(by: .byViews) // 조회수랑 같게 함
+                                }
+                                .padding(5)
+                                .background(selectedButton == 2 ? Color.accentColor : Color.clear)
+                                .foregroundColor(selectedButton == 2 ? .white : .black)
+                                .cornerRadius(10)
+                                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.accentColor, lineWidth: 1))
+                                
+                                Button("난이도 높은순") {
+                                    selectedButton = 3
+                                    viewModel.sortRanks(by: .byDifficulty) // 조회수랑 같게 함
+                                }
+                                .padding(5)
+                                .background(selectedButton == 3 ? Color.accentColor : Color.clear)
+                                .foregroundColor(selectedButton == 3 ? .white : .black)
+                                .cornerRadius(10)
+                                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.accentColor, lineWidth: 1))
+                                
+                                Button("난이도 낮은순") {
+                                    selectedButton = 4
+                                    viewModel.sortRanks(by: .byEasy) // 조회수랑 같게 함
+                                }
+                                .padding(5)
+                                .background(selectedButton == 4 ? Color.accentColor : Color.clear)
+                                .foregroundColor(selectedButton == 4 ? .white : .black)
+                                .cornerRadius(10)
+                                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.accentColor, lineWidth: 1))
+                                
+                                Spacer()
+                            }
                         }
-                        .padding(5)
-                        .background(selectedButton == 2 ? customGray : Color.clear)
-                        .foregroundColor(selectedButton == 2 ? .white : .black)
-                        .cornerRadius(10)
-                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 1))
-                        
-                        Button("난이도 높은순") {
-                            selectedButton = 3
-                            viewModel.sortRanks(by: .byDifficulty) // 조회수랑 같게 함
-                        }
-                        .padding(5)
-                        .background(selectedButton == 3 ? customGray : Color.clear)
-                        .foregroundColor(selectedButton == 3 ? .white : .black)
-                        .cornerRadius(10)
-                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 1))
-                        
-                        Button("난이도 낮은순") {
-                            selectedButton = 4
-                            viewModel.sortRanks(by: .byEasy) // 조회수랑 같게 함
-                        }
-                        .padding(5)
-                        .background(selectedButton == 4 ? customGray : Color.clear)
-                        .foregroundColor(selectedButton == 4 ? .white : .black)
-                        .cornerRadius(10)
-                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 1))
-                        
-                        Spacer()
-                        
                     }
                     .padding(.horizontal)
-                }
+                
                 
                 // 랭킹 리스트 뷰
                 HomeRankingListView(viewModel: viewModel, showShadowAndSeparator: false)
