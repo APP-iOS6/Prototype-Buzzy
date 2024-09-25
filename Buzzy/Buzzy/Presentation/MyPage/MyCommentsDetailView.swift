cb94c37c4bad7f66a14c1fb05d5d761cae4295da
Min <jmsj0308@naver.com>
2024-09-25 15:25:33 +0900

    add: MyCommentsDetailView 파일 추가

---
+++ Buzzy/Buzzy/Presentation/MyPage/MyCommentsDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MyCommentsDetailView.swift
+//  Buzzy
+//
+//  Created by Min on 9/25/24.
+//
+
+import SwiftUI
+
+struct MyCommentsDetailView: View {
+    var body: some View {
+        Text("123")
+    }
+}
+
+#Preview {
+    MyCommentsDetailView()
+}
