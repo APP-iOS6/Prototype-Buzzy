19572b5b6e24dd8ba7b3e9dac08d2f8a6a5c0e01
Min <jmsj0308@naver.com>
2024-09-25 14:06:37 +0900

    add: MyPostDetailView 파일 추가

---
+++ Buzzy/Buzzy/Presentation/MyPage/MyPostDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  MyPostDetailView.swift
+//  Buzzy
+//
+//  Created by Min on 9/25/24.
+//
+
+import SwiftUI
+
+struct MyPostDetailView: View {
+    var body: some View {
+        VStack {
+            Text("MyPostDetailView")
+        }
+    }
+}
+
+#Preview {
+    MyPostDetailView()
+}
